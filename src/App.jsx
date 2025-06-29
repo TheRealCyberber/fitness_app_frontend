3a3cd3d4ca39e3083217c9b9dd839fbcc138fffd
ShahlaaBee <shahlaa.ah@gmail.com>
2025-06-29 17:01:34 +0300

    added signout function to auth.js, updated app.jsx so that it clears the list after signout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { CheckSession } from './services/Auth'
+import { SignOutUser } from './services/Auth'
 import Nav from './components/Nav'
 import Register from './pages/Register'
 import SignIn from './pages/SignIn'
@@ -12,11 +13,15 @@ import './App.css'
 
 const App = () => {
   const [user, setUser] = useState(null)
+  const [progress, setProgress] = useState([])
+  const [workouts, setWorkouts] = useState([])
 
   
   const handleLogOut = () => {
+    SignOutUser()
     setUser(null)
-    localStorage.clear()
+    setProgress([])
+    setWorkouts([])
   }
   
   const checkToken = async () => {
@@ -36,12 +41,35 @@ const App = () => {
       <Nav user={user} handleLogOut={handleLogOut} />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn setUser={setUser} />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/workouts" element={<WorkoutList />} />
-          <Route path="/progress" element={<ProgressList />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register setUser={setUser} />} />
+        <Route path="/signin" element={<SignIn setUser={setUser} />} />
+        <Route
+          path="/dashboard"
+          element={
+            <Dashboard user={user} />
+          }
+        />
+        <Route
+          path="/workouts"
+          element={
+            user ? (
+              <WorkoutList workouts={workouts} setWorkouts={setWorkouts} />
+            ) : (
+              <SignIn setUser={setUser} />
+            )
+          }
+        />
+        <Route
+          path="/progress"
+          element={
+            user ? (
+              <ProgressList progress={progress} setProgress={setProgress} />
+            ) : (
+              <SignIn setUser={setUser} />
+            )
+          }
+        />
         </Routes>
       </main>
     </>
